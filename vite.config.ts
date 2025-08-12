3a3b30b463212df14ac4db24d66737962664b943
L0rBl4k <lorblak@AppleM1.local>
2025-08-11 23:36:59 -0300

    chore(vite.config.ts) upgraded vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2025-hb/'
+  base: '/2025-hb/',
+  build: {
+    assetsDir: 'assets',
+    copyPublicDir: true,
+  }
 })
