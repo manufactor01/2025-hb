af1f10f1bb27b032150dd5ef66b9c092e3217d47
L0rBl4k <lorblak@AppleM1.local>
2025-08-11 23:54:32 -0300

    chore: solucion final

--- src/component/Wishsection.tsx
+++ src/component/Wishsection.tsx
@@ -1,24 +1,37 @@
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
 
 function Wishsection() {
+  const backgroundImage = new URL('../../public/4.png', import.meta.url).href;
 
+  return (
+    <>
+      <div className="h-screen bg-[#023047] flex flex-col items-center justify-center">
+        <h3 className="font-mukta text-[#8ECAE6] text-6xl font-semibold m-8 max-sm:text-3xl">
+          🎊 FELIZ CUMPLE ✨
+        </h3>
 
-
-    return <><div className='h-screen bg-[#023047] flex flex-col items-center justify-center'>
-        <h3 className="font-mukta text-[#8ECAE6] text-6xl font-semibold m-8 max-sm:text-3xl">🎊 FELIZ CUMPLE ✨</h3>
-
-        <div className="w-[349px] h-[314px] rounded max-sm:w-[225px] max-sm:h-[250px] bg-cover" style={{ backgroundImage: `url("${import.meta.env.BASE_URL}4.png")` }}></div>
-        <h1 className="font-mukta text-white text-8xl font-bold m-5 max-sm:text-4xl"><Typewriter
+        <div
+          className="w-[349px] h-[314px] rounded max-sm:w-[225px] max-sm:h-[250px] bg-cover"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        ></div>
+        <h1 className="font-mukta text-white text-8xl font-bold m-5 max-sm:text-4xl">
+          <Typewriter
             options={{
-                strings: ['<span style="color: #29E7CD;">Lore </span>', ' <span style="color: #F0F0C9;">Lorena</span>', '<span style="color: #D4F5F5;">Lorena</span>'],
-                delay: 200,
-                autoStart: true,
-                loop: true,
-                cursor: ''
+              strings: [
+                '<span style="color: #29E7CD;">Lore </span>',
+                ' <span style="color: #F0F0C9;">Lorena</span>',
+                '<span style="color: #D4F5F5;">Lorena</span>',
+              ],
+              delay: 200,
+              autoStart: true,
+              loop: true,
+              cursor: "",
             }}
-        /></h1>
-    </div>
+          />
+        </h1>
+      </div>
     </>
+  );
 }
 
-export default Wishsection
+export default Wishsection;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/2025-hb/',
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
     copyPublicDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   }
 })
